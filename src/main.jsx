b6892fe7e0899bc648c8ca31f08d7fc8bc7487e2
DrieleMoreira <driele.moreira@gmail.com>
2025-05-06 19:21:02 -0300

    main

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
-import { StrictMode } from "react";
+
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import Router from "./routes/routes.jsx";
 import GlobalStyle from "./styles/GlobalStyles.js";
+import { StrictMode } from "react";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
