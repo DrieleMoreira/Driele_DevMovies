c78882ccd3b935990dc4e2564226ae00ad988b71
DrieleMoreira <driele.moreira@gmail.com>
2025-05-06 20:28:09 -0300

    alteração bug

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,13 @@
-
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import Router from "./routes/routes.jsx";
+import Router from "../src/routes/routes.jsx";
 import GlobalStyle from "./styles/GlobalStyles.js";
-import { StrictMode } from "react";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
+  <>
     <BrowserRouter basename="/Driele_DevMovies">
       <Router />
     </BrowserRouter>
     <GlobalStyle />
-  </StrictMode>
+  </>
 );
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -8,7 +8,7 @@ import Detail from "../containers/Detail"
 
 function Router() {
   return (
-    <Routes basename="/Driele_DevMovies">
+    <Routes>
       <Route element={<DefaultLayout/>}>
       <Route path="/" element={<Home />} />
       <Route path="/filmes" element={<Movies />} />
