4a5eb3edabe3db7eedcb65eb36b8f68ff4a696b4
DrieleMoreira <driele.moreira@gmail.com>
2025-05-07 12:23:04 -0300

    global

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import Router from "../src/routes/routes.jsx";
-import GlobalStyles from "./styles/GlobalStyles.js";
+import GlobalStyles from './styles/GlobalStyles.js'
 import { StrictMode } from "react";
 
 createRoot(document.getElementById("root")).render(
