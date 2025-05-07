799c092bdae29eba4f713e2592c6ecccd80a0b26
DrieleMoreira <driele.moreira@gmail.com>
2025-05-07 11:55:28 -0300

    main

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,13 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import Router from "../src/routes/routes.jsx";
 import GlobalStyles from "./styles/GlobalStyles.js";
+import { StrictMode } from "react";
 
 createRoot(document.getElementById("root")).render(
-  <>
+  <StrictMode>
     <BrowserRouter basename="/Driele_DevMovies">
       <Router />
     </BrowserRouter>
     <GlobalStyles />
-  </>
+  </StrictMode>
 );
